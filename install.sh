8141acb5d10183f34ef0d234e416b1851ccbaede
bechir <bechir@bechir.prologue.prl>
2016-10-12 10:20:37 +0200

    first config

---
+++ install.sh
@@ -0,0 +1,20 @@
+###install script
+
+
+###install packet with a user XXXX
+$USER=XXXX
+
+#install npm, bower, php-mycrypt
+
+
+git clone XXXXX medolutioniot
+
+
+
+cd medolutioniot
+
+composer install
+npm install
+bower install
+gulp watch
+php artisan cache:clear
