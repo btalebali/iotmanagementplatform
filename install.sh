e1e18693a13aeb8319afe850eb1eed8a72afd55c
Béchir TALEB ALI <btalebali@prologue.fr>
2016-10-17 09:57:15 +0200

    updates

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ export USER=cloud
 export MEDOLUTION_IOT_VERSION=0.0.0-beta
 export BITBUCKET_USER=Bechir
 export BITBUCKET_PASSWORD=Ta122016$
-
+export DEBIAN_FRONTEND="noninteractive"
 
 export BRANCH=master
 sudo apt-get install -y realpath
@@ -47,38 +47,64 @@ sudo rm /etc/apache2/sites-enabled/*
 sudo cp $DIR_MEDOLUTION/medolutioniot/conf/medolution_apache.conf /etc/apache2/sites-available/medolution.conf
 sudo sed -i "s|DIR_MEDOLUTION|$DIR_MEDOLUTION|" /etc/apache2/sites-available/medolution.conf
 
-
-sudo cat >> /etc/apache2/apache2.conf << END
+sudo chmod 666 /etc/apache2/apache2.conf
+cat >> /etc/apache2/apache2.conf << END
 <Directory $DIR_MEDOLUTION>
   Options Indexes FollowSymLinks
   AllowOverride All
   Require all granted
 </Directory>
 END
+
+sudo chmod 660 /etc/apache2/apache2.conf
 ###end conf file
 sudo a2ensite medolution
 sudo service apache2 restart
 
 
+###Composer install
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/bin --filename=composer
+
+
+#### MariaDB server
+export DEBIAN_FRONTEND="noninteractive"
+
+MARIADB_VERSION='10.1'
+# Import repo key
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+
+# Add repo for MariaDB
+sudo add-apt-repository "deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/$MARIADB_VERSION/ubuntu trusty main"
+
+# Update
+sudo apt-get update
+# Install MariaDB without password prompt
+sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password password $MYSQL_ADMIN_PASSWD"
+sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password_again password $MYSQL_ADMIN_PASSWD"
+
+sudo apt-get install -qq mariadb-server
+
+#####################Inject root password ################################
+MYSQL=`which mysql`
+Q1="use mysql;"
+Q2="update user set plugin='' where User='root';"
+Q3="update user set Password=PASSWORD('$MYSQL_ADMIN_PASSWD') where User='root';"
+Q4="GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWD' WITH GRANT OPTION;"
+Q5="FLUSH PRIVILEGES;"
+
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}"
+
+$MYSQL -uroot -p$MYSQL_ADMIN_PASSWD -e "$SQL"
+
+service mysql restart
 
 
 
 
 
-##Database
 
 
-sudo apt-get install -y mysql-server
-
-
-
-mysql -f -uroot -p$MYSQL_ADMIN_PASSWD -e "create database $DB_DATABASE;"
-mysql -f -uroot -p$MYSQL_ADMIN_PASSWD -e "CREATE USER '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -f -uroot -p$MYSQL_ADMIN_PASSWD -e "GRANT ALL PRIVILEGES ON uicb.* TO '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';FLUSH PRIVILEGES;"
-
-
-
-cd $INST_SCRIPT_PATH
+cd $DIR_MEDOLUTION/medolutioniot
 chown -R www-data:www-data storage bootstrap/cache
 
 cp .env.example .env
@@ -86,9 +112,6 @@ cp .env.example .env
 sed -i "s/DB_DATABASE=/DB_DATABASE=$DB_DATABASE/" .env
 sed -i "s/DB_USERNAME=/DB_USERNAME=$DB_USERNAME/" .env
 sed -i "s/DB_PASSWORD=/DB_PASSWORD=$DB_PASSWORD/" .env
-
-###Composer install 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/bin --filename=composer
 
 
 
