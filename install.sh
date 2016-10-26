65c51d626595a73a0a7752a63957474520e1cbf1
Bechir TALEB ALI <btalebali@prologue.fr>
2016-10-26 21:33:45 +0200

    updates

--- install.sh
+++ install.sh
@@ -104,6 +104,7 @@ sudo service apache2 restart
 
 
 ###Composer install
+sudo apt-get install -y curl 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/bin --filename=composer
 
 
